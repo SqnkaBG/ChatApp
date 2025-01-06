78243a240c57b55260c0669ace212626001617ee
SqnkaBG - БГ Сянка <100valentin100@gmail.com>
2025-01-06 16:38:29 +0200

    ass me

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,8 +1,7 @@
+import SearchBar from "@/components/searchbar";
+import Sidebar from "@/components/sidebar";
 import Image from "next/image";
 
-export default function Test() 
-{
-    //converstaions
-    //user data
-    //settings (config)
+export default function Test() {
+  return <div className="flex h-screen w-screen flex-col bg-gray-500"></div>;
 }
