2fdcbea3f9dc7764f6521f6aead1c7d221e51d3a
ConArtist35 <wollfeyfox@gmail.com>
2025-01-07 22:07:52 +0200

    friends 2

--- app/friends/page.tsx
+++ app/friends/page.tsx
@@ -11,26 +11,31 @@ export default function Friends() {
       <div className="h-full w-[80%] flex-col items-center justify-center bg-teal-700 sm:w-[95.5%]">
         <SearchBar />
         <div className="relative flex h-[93%] w-[100%] bg-gray-300 space-x-12 space-y-12 justify-center flex-wrap content-start">
-          <h1 className="w-full text-center mt-[1%] text-3xl text-black">Friends</h1>
-          <div className="flex h-[11%] w-[30%] bg-gray-200 box-border border border-black">
+          <h1 className="w-full text-center mt-[1%] text-4xl text-black">Friends</h1>
+          <div className="flex h-[11%] w-[20%] bg-gray-200 box-border border border-black">
            
           </div>
-          <div className="flex h-[11%] w-[30%] bg-gray-200 box-border border border-black">
+          <div className="flex h-[11%] w-[20%] bg-gray-200 box-border border border-black">
 
           </div>
-          <div className="flex h-[11%] w-[30%] bg-gray-200 box-border border border-black">
+          <div className="flex h-[11%] w-[20%] bg-gray-200 box-border border border-black">
 
           </div>
-          <div className="flex h-[11%] w-[30%] bg-gray-200 box-border border border-black">
+          <div className="flex h-[11%] w-[20%] bg-gray-200 box-border border border-black">
 
           </div>
-          <div className="flex h-[11%] w-[30%] bg-gray-200 box-border border border-black">
+          <div className="flex h-[11%] w-[20%] bg-gray-200 box-border border border-black">
 
           </div>
-          <div className="flex h-[11%] w-[30%] bg-gray-200 box-border border border-black">
+          <div className="flex h-[11%] w-[20%] bg-gray-200 box-border border border-black">
 
           </div>
-          
+          <div className="flex h-[11%] w-[20%] bg-gray-200 box-border border border-black">
+
+          </div>
+          <div className="flex h-[11%] w-[20%] bg-gray-200 box-border border border-black">
+
+          </div>
         </div>
       </div>
     </div>
