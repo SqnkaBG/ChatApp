7cb8b93be933568f4acbf0a5b54100eb25e7cc2b
SqnkaBG - БГ Сянка <63199755+SqnkaBG@users.noreply.github.com>
2025-01-07 22:11:55 +0200

    minor change in friends

--- app/friends/page.tsx
+++ app/friends/page.tsx
@@ -5,37 +5,23 @@ import Cat from "@/app/Images/cat-circle.png";
 
 export default function Friends() {
   return (
-    <div className="flex h-screen w-screen g-white">
+    <div className="g-white flex h-screen w-screen">
       <Sidebar />
-    
+
       <div className="h-full w-[80%] flex-col items-center justify-center bg-teal-700 sm:w-[95.5%]">
         <SearchBar />
-        <div className="relative flex h-[93%] w-[100%] bg-gray-300 space-x-12 space-y-12 justify-center flex-wrap content-start">
-          <h1 className="w-full text-center mt-[1%] text-4xl text-black">Friends</h1>
-          <div className="flex h-[11%] w-[20%] bg-gray-200 box-border border border-black">
-           
-          </div>
-          <div className="flex h-[11%] w-[20%] bg-gray-200 box-border border border-black">
-
-          </div>
-          <div className="flex h-[11%] w-[20%] bg-gray-200 box-border border border-black">
-
-          </div>
-          <div className="flex h-[11%] w-[20%] bg-gray-200 box-border border border-black">
-
-          </div>
-          <div className="flex h-[11%] w-[20%] bg-gray-200 box-border border border-black">
-
-          </div>
-          <div className="flex h-[11%] w-[20%] bg-gray-200 box-border border border-black">
-
-          </div>
-          <div className="flex h-[11%] w-[20%] bg-gray-200 box-border border border-black">
-
-          </div>
-          <div className="flex h-[11%] w-[20%] bg-gray-200 box-border border border-black">
-
-          </div>
+        <div className="relative flex h-[93%] w-[100%] flex-wrap content-start justify-center space-x-12 space-y-12 bg-gray-300">
+          <h1 className="mt-[1%] w-full text-center text-4xl text-black">
+            Friends
+          </h1>
+          {Array.from({ length: 12 }, (_, index) => {
+            return (
+              <div
+                key={index}
+                className="box-border flex h-[11%] w-[20%] border border-black bg-gray-200"
+              ></div>
+            );
+          })}
         </div>
       </div>
     </div>
