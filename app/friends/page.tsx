e2e14dbe43a130e4a70f0bcd9f8f1bf160f942e7
ConArtist35 <wollfeyfox@gmail.com>
2025-01-07 21:59:14 +0200

    friends

--- app/friends/page.tsx
+++ app/friends/page.tsx
@@ -1,16 +1,37 @@
 import SearchBar from "@/components/searchbar";
-import ShowBox from "@/components/showbox";
 import Sidebar from "@/components/sidebar";
+import Image from "next/image";
+import Cat from "@/app/Images/cat-circle.png";
 
 export default function Friends() {
   return (
-    <div className="flex h-screen w-screen flex-wrap bg-white">
+    <div className="flex h-screen w-screen g-white">
       <Sidebar />
     
       <div className="h-full w-[80%] flex-col items-center justify-center bg-teal-700 sm:w-[95.5%]">
         <SearchBar />
-        <ShowBox />
-        
+        <div className="relative flex h-[93%] w-[100%] bg-gray-300 space-x-12 space-y-12 justify-center flex-wrap content-start">
+          <h1 className="w-full text-center mt-[1%] text-3xl text-black">Friends</h1>
+          <div className="flex h-[11%] w-[30%] bg-gray-200 box-border border border-black">
+           
+          </div>
+          <div className="flex h-[11%] w-[30%] bg-gray-200 box-border border border-black">
+
+          </div>
+          <div className="flex h-[11%] w-[30%] bg-gray-200 box-border border border-black">
+
+          </div>
+          <div className="flex h-[11%] w-[30%] bg-gray-200 box-border border border-black">
+
+          </div>
+          <div className="flex h-[11%] w-[30%] bg-gray-200 box-border border border-black">
+
+          </div>
+          <div className="flex h-[11%] w-[30%] bg-gray-200 box-border border border-black">
+
+          </div>
+          
+        </div>
       </div>
     </div>
   );
--- app/user_account/page.tsx
+++ app/user_account/page.tsx
@@ -7,77 +7,12 @@ export default function Account() {
   return (
     <div className="flex h-screen w-screen flex-wrap bg-white">
       <Sidebar />
-
       <div className="h-full w-[80%] flex-col items-center justify-center bg-teal-700 sm:w-[95.5%]">
         <SearchBar />
         <div className="b-0 flex-col h-[93%] w-[100%] bg-blue-300 flex items-center"> 
-          <h2 className="text-xl mt-[1%] text-black">Today</h2>
-          <div className="box-content h-[15%] w-[70%] bg-slate-800 mt-[0.5%] opacity-80 hover:opacity-100 flex items-center">
-            <div className="ml-5 flex flex-wrap">
-                    <Image
-                      src={Cat}
-                      alt="pfp"
-                      width={50}
-                      height={50}
-                      className="py-2"></Image>
-            </div>
-            <p className="ml-[1%]"><b>Niggerfaggot</b> sent you a message.</p>
-            <p className="text-sm ml-[64%]">5 minutes ago</p>
-          </div>
-          <div className="box-content h-[15%] w-[70%] bg-slate-800 mt-[0.5%] opacity-80 hover:opacity-100 flex items-center">
-            <div className="ml-5 flex flex-wrap">
-                      <Image
-                        src={Cat}
-                        alt="pfp"
-                        width={50}
-                        height={50}
-                        className="py-2"></Image>
-              </div>
-              <p className="ml-[1%]"><b>Niggerfaggot</b> sent you a message.</p>
-              <p className="text-sm ml-[64%]">15 minutes ago</p>
-          </div>
-          <div className="box-content h-[15%] w-[70%] bg-slate-800 mt-[0.5%] opacity-80 hover:opacity-100 flex items-center">
-            <div className="ml-5 flex flex-wrap">
-                      <Image
-                        src={Cat}
-                        alt="pfp"
-                        width={50}
-                        height={50}
-                        className="py-2"></Image>
-              </div>
-              <p className="ml-[1%]"><b>Niggerfaggot</b> sent you a message.</p>
-              <p className="text-sm ml-[66%]">1 hour ago</p>
-          </div>
-          <h2 className="text-xl mt-[1%] text-black">Yesterday</h2>
-          <div className="box-content h-[15%] w-[70%] bg-slate-800 mt-[0.5%] opacity-80 hover:opacity-100 flex items-center">
-            <div className="ml-5 flex flex-wrap">
-                    <Image
-                      src={Cat}
-                      alt="pfp"
-                      width={50}
-                      height={50}
-                      className="py-2"></Image>
-            </div>
-            <p className="ml-[1%] overflow-hidden "><b>Niggerfaggot</b> updated their status: "God I fucking hate niggers so much. I wish I could exterminate them. Sadly it turned out that I am 30% black and am about to commit suicide."</p>
-            <p className="ml-[1%] mr-[1%] text-sm">26.12.2024</p>
-          </div>
-          <div className="box-content h-[15%] w-[70%] bg-slate-800 mt-[0.5%] opacity-80 hover:opacity-100 flex items-center">
-          <div className="ml-5 flex flex-wrap">
-                    <Image
-                      src={Cat}
-                      alt="pfp"
-                      width={50}
-                      height={50}
-                      className="py-2"></Image>
-            </div>
-            <p className="ml-[1%] overflow-hidden "><b>Niggerfaggot</b> has sent you a friend request</p>
-            <p className="ml-[61.5%] text-sm">26.12.2024</p>
-          </div>
+          
         </div>
       </div>
     </div>
   );
 }
-//trqbva da se addne scrollbar
-//cvetovete sa grozni trqbva da se smenqt
-//button read all notifications da se addne i kato se natisne da se napravi opacityto da e 50%
