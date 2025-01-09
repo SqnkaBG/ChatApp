bcd91ab0fec6890e8326288aece47d4d346068e1
ConArtist35 <wollfeyfox@gmail.com>
2025-01-09 19:33:34 +0200

    account page changes

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,7 +1,28 @@
-import SearchBar from "@/components/searchbar";
-import Sidebar from "@/components/sidebar";
-import Image from "next/image";
+'use client';
+import React, { useState } from 'react';
 
 export default function Test() {
-  return <div className="flex h-screen w-screen flex-col bg-gray-500"></div>;
+  const [text, setText] = useState("User1");
+  const [inputText, setInputText] = useState("User1");  // temp var so that the text doesn't change without pressing the button
+
+  const handleChange = () => {
+    setText(inputText);
+  };
+  return (
+    <div className="flex flex-col h-screen w-screen bg-gray-500 justify-center items-center space-y-3">
+      <input
+      type="text"
+      className="flex h-[5%] w-[10%] text-black text-center"
+      value={inputText}
+      onChange={(e) => setInputText(e.target.value)}
+      />
+      <p className="flex text-lg text-black">{text}</p>
+      <button
+                type="button"
+                className="ml-2 h-10 w-[10%] rounded-lg bg-blue-600 px-4 py-2 text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                onClick={handleChange}>
+                Change
+      </button>
+    </div>
+  );
 }
--- app/user_account/page.tsx
+++ app/user_account/page.tsx
@@ -9,8 +9,54 @@ export default function Account() {
       <Sidebar />
       <div className="h-full w-[80%] flex-col items-center justify-center bg-teal-700 sm:w-[95.5%]">
         <SearchBar />
-        <div className="b-0 flex-col h-[93%] w-[100%] bg-blue-300 flex items-center"> 
-          
+        <div className="b-0 flex-col h-[93%] w-[100%] bg-blue-300 flex items-center justify-start space-y-1"> 
+          <div className="flex flex-row h-[18%] w-[40%] space-x-6 items-center justify-center">
+            <Image
+              src={Cat}
+              alt="pfp"
+              height={100}
+              width={100}
+              className="mt-[4%]"
+            />
+            <h1 className="text-center text-4xl text-black mt-[4%]"><b>User1</b></h1>
+          </div>
+          <hr className="w-[40%] color-black"/>
+          <div className="flex flex-row h-[8%] w-[30%] space-x-5 items-center">
+            <p className="text-lg text-black">Username</p>
+            <input
+                type="text"
+                className="h-7 flex-grow text-black text-lg outline-double" 
+                value=" User1"
+              />
+          </div>
+          <div className="flex flex-row h-[8%] w-[30%] space-x-5 items-center">
+            <p className="text-lg text-black">Phone Number</p>
+            <input
+                type="text"
+                className="h-7 flex-grow text-black text-lg outline-double" 
+                value=" 088-111-1111"
+              />
+          </div>
+          <div className="flex flex-row h-[8%] w-[30%] space-x-5 items-center">
+            <p className="text-lg text-black">New password</p>
+            <input
+                type="password"
+                className="h-7 flex-grow text-black text-lg outline-double" 
+              />
+          </div>
+          <hr className="w-[40%] color-black"/>
+          <div className="flex flex-row h-[8%] w-[30%] space-x-5 items-center ">
+            <p className="text-lg text-black">Description</p>
+            <textarea className="border-box resize-none text-black flex-grow outline-double" ></textarea>
+          </div>
+          <div className="flex flex-row h-[8%] w-[30%] space-x-5 items-center">
+            <p className="text-lg text-black">Status</p>
+            <input
+                type="text"
+                className="h-7 text-black text-lg outline-double flex-grow rounded-xl pl-[1%]" 
+                placeholder="Enter status"
+              />
+          </div>
         </div>
       </div>
     </div>
