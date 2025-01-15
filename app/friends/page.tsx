cee1c3ffd9b9cbdcf4816e6dc0dca19bb21eef1d
ConArtist35 <wollfeyfox@gmail.com>
2025-01-15 22:18:06 +0200

    changes to user settings

--- app/friends/page.tsx
+++ app/friends/page.tsx
@@ -3,8 +3,6 @@ import Sidebar from "@/components/sidebar";
 import Image from "next/image";
 import Cat from "@/app/Images/cat-circle.png";
 import { AiOutlineMessage } from "react-icons/ai";
-import { MdOutlinePersonRemove } from "react-icons/md";
-import { ImBlocked } from "react-icons/im";
 
 export default function Friends() {
   return (
@@ -12,40 +10,25 @@ export default function Friends() {
       <Sidebar/>
 
       <div className="h-full w-full flex-col items-center justify-center bg-teal-700 sm:w-[95.5%]">
-        <SearchBar />
-        <div className="relative flex flex-col lg:flex-row h-[93%] w-full lg:flex-wrap content-start lg:justify-center md:space-x-12 space-y-2 md:space-y-12 bg-gray-300 overflow-y-auto pb-[20%] lg:pb-[5%]">
+        <SearchBar/>
+        <div className="flex flex-col lg:flex-row h-[93%] w-full lg:flex-wrap lg:justify-center md:space-x-12 space-y-2 md:space-y-12 bg-gray-300 overflow-y-auto pb-[20%] lg:pb-[5%]">
           <h1 className="lg:mt-[1.3%] mt-[4%] text-center w-full text-4xl text-black">Friends</h1>
-          <button
-                type="button"
-                className="hidden lg:block absolute top-10 lg:right-[32%] h-10 rounded-lg bg-blue-600 px-4 py-2 text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
-          >Search</button>
-          <input
-            className="absolute top-[11%] md:top-[6.5%] lg:top-12 left-[15%] md:left-[16.5%] lg:left-[35%] w-[70%] lg:w-full max-w-md bg-gray-200 text-black text-center placeholder:text-center placeholder:text-black italic outline-none focus:placeholder:text-gray-200"
-            type="text"
-            placeholder="Search friends by name"
-          ></input>
           
           {Array.from({ length: 48 }, (_, index) => {
             return (
               <div
                 key={index}
-                className="relative top-16 left-[7%] md:left-0 flex flex-none box-border h-[15%] w-[87%] lg:h-[11%] lg:w-[20%] lg:border border-black lg:bg-blue-200 active:bg-gray-200 active:bg-opacity-75 rounded-3xl items-center"
-              ><Image
+                className="ml-[5%] lg:mt-0 flex flex-none box-border h-[15%] w-[90%] lg:h-[11%] lg:w-[20%] lg:border border-black lg:bg-blue-200 active:bg-gray-200 active:bg-opacity-75 rounded-3xl items-center">
+                  <Image
                     src={Cat}
                     alt="pfp"
                     width={55}
-                    className="lg:object-scale-down md:w-[20%] lg:h-[65%] w-[36%] lg:w-[25%] ml-[3%]"
-                  />
+                    className="lg:object-scale-down md:w-[20%] lg:h-[65%] w-[36%] lg:w-[25%] ml-[3%]"/>
                   <p className="text-black ml-[5%] lg:ml-[3%] text-l md:text-3xl lg:text-xl">User1</p>
                   <div className="flex items-center h-full w-full justify-end lg:pr-[5%]">
-                    <MdOutlinePersonRemove  className="h-[38%] w-[18%] lg:w-[14%] fill-gray-600"
-                      title="Remove from Friends List"/>
-                    <ImBlocked className="h-[23%] md:h-[30%] w-[23%] md:w-[18%] fill-gray-600 lg:mr-[0.9%]"
-                      title="Block User"/>
                     <a
                         className="flex h-[28%] w-[18%] md:w-[15%] md:h-[35%] lg:h-[40%] lg:w-[14%] md:ml-[2%] mr-[3%] lg:mr-0 lg:ml-0"
-                          href="/dms">
-                              
+                          href="/dms"> 
                         <AiOutlineMessage className="h-full w-full fill-gray-600"
                           title="Send message"/>
                     </a>
--- app/user_account/page.tsx
+++ app/user_account/page.tsx
@@ -2,68 +2,60 @@ import SearchBar from "@/components/searchbar";
 import Sidebar from "@/components/sidebar";
 import Image from "next/image";
 import Cat from "@/app/Images/cat-circle.png";
+import { FaPencilAlt } from "react-icons/fa";
 
 export default function Account() {
   return (
     <div className="flex h-screen w-screen flex-wrap bg-white">
       <Sidebar />
-      <div className="h-full w-[80%] flex-col items-center justify-center bg-teal-700 sm:w-[95.5%]">
+      <div className="h-full w-full flex-col items-center justify-center bg-teal-700 sm:w-[95.5%]">
         <SearchBar />
-        <div className="b-0 flex-col h-[93%] w-[100%] bg-blue-300 flex items-center justify-start space-y-1"> 
-          <div className="flex flex-row h-[18%] w-[40%] space-x-6 items-center justify-center">
+        <div className="b-0 flex-col h-[93%] w-[100%] bg-blue-300 flex items-center space-y-1 pb-[2%] overflow-y-auto"> 
+          <div className="flex flex-row h-[18%] w-[40%] items-center justify-center mb-[1%]">
             <Image
               src={Cat}
               alt="pfp"
-              width={100}
-              className="mt-[4%]"
-            />
-            <h1 className="text-center text-4xl text-black mt-[4%]"><b>User1</b></h1>
+              height={0}
+              width={90}
+              className="mt-[4%] lg:w-100"/>
+            <label htmlFor="file-upload" className="cursor-pointer text-gray-200 hover:text-gray-400 mb-[6%] ">
+                <FaPencilAlt className=""/>
+            </label>
+            <input type="file" id="file-upload" className="hidden"/>
+            <h1 className="text-center text-4xl text-black mt-[4%] ml-[2%]"><b>User1</b></h1>
           </div>
-          <hr className="w-[40%] color-black"/>
-          <div className="flex flex-row h-[8%] w-[30%] space-x-5 items-center">
-            <p className="text-lg text-black">Username</p>
-            <input
-                type="text"
-                className="h-7 flex-grow text-black text-lg outline-double" 
-                alt="User1"
-              />
+          <hr className="w-[40%]"/>
+          <div className="h-full w-[40%] flex flex-col space-y-12 items-center overflow-y-auto pt-[1%] pb-[1%]">
+            {Array.from({ length: 48 }, (_, index) => {
+            return (
+              <div
+                key={index}
+                className="flex flex-row h-auto w-[90%] space-x-5 items-center justify-center">
+                  <label className="text-lg text-black">{`Setting ${index + 1}`}</label>
+                  <input
+                  type="text"
+                  className="h-7 w-[50%] text-black text-lg outline-double pl-[0.5%]"/>
+              </div>
+              );
+            })}
           </div>
-          <div className="flex flex-row h-[8%] w-[30%] space-x-5 items-center">
-            <p className="text-lg text-black">Phone Number</p>
-            <input
-                type="text"
-                className="h-7 flex-grow text-black text-lg outline-double" 
-              />
-          </div>
-          <div className="flex flex-row h-[8%] w-[30%] space-x-5 items-center">
-            <p className="text-lg text-black">New password</p>
-            <input
-                type="password"
-                className="h-7 flex-grow text-black text-lg outline-double" 
-              />
-          </div>
-          <hr className="w-[40%] color-black"/>
+          <hr className="w-[40%] mt-[20%]"/>
           <div className="flex flex-row h-[8%] w-[30%] space-x-5 items-center pt-[2%]">
             <p className="text-lg text-black">Description</p>
             <textarea className="border-box resize-none text-black flex-grow outline-double" ></textarea>
           </div>
-          <div className="flex flex-row h-[8%] w-[30%] space-x-5 space-y-8 items-center">
+          <div className="flex flex-row h-[8%] w-[30%] space-x-5 space-y-8 justify-end">
             <p className="text-lg text-black mt-[5.5%]">Status</p>
             <input
                 type="text"
-                className="h-7 text-black text-lg outline-double flex-grow rounded-xl pl-[1%]" 
-                placeholder="Enter status"
-              />
+                className="h-7 text-black text-lg outline-double  rounded-xl pl-[1%] flex-grow" 
+                placeholder="Enter status"/>
           </div>
-            <div className="flex flex-row w-[20%] h-[10%] justify-center space-x-4 ml-[1%]">
+            <div className="flex flex-row w-[20%] h-[10%] justify-center ml-[1%] pt-[1%]">
               <button
                 type="button"
-                className="mt-[5%] h-10 rounded-lg bg-blue-600 px-4 py-2 text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
-              >Save changes</button> 
-              <button
-                type="button"
-                className="mt-[5%] h-10 rounded-lg bg-blue-600 px-4 py-2 text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
-              >Log out</button>
+                className="mt-[5%] h-10 rounded-lg bg-blue-600 px-4 py-2 text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 "
+                >Save changes</button>
             </div>
         </div>
       </div>
