8781b5c8f28f332e1bcc94dff5c0257d372e5f26
SqnkaBG - БГ Сянка <63199755+SqnkaBG@users.noreply.github.com>
2025-01-20 20:35:55 +0200

    updates

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,14 @@
-import globals from "globals";
 import pluginJs from "@eslint/js";
+import globals from "globals";
 import tseslint from "typescript-eslint";
-
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"],
-   languageOptions: {sourceType: "script"}},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+    {
+        files: ["**/*.{js,mjs,cjs,ts}"],
+        languageOptions: { sourceType: "script" },
+    },
+    { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
 ];
