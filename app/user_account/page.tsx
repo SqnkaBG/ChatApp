f0d678fc798308e99ee6163d1ecb7297163a12e9
ConArtist35 <wollfeyfox@gmail.com>
2025-01-24 22:02:11 +0200

    account pic can change!!!!

--- app/user_account/page.tsx
+++ app/user_account/page.tsx
@@ -1,10 +1,22 @@
+"use client";
+
 import Cat from "@/app/Images/cat-circle.png";
 import SearchBar from "@/components/searchbar";
 import Sidebar from "@/components/sidebar";
 import Image from "next/image";
+import React, { useState } from "react";
 import { FaPencilAlt } from "react-icons/fa";
 
 export default function Account() {
+    const [imageSrc, setImageSrc] = useState<string>(Cat.src); // Initial image source
+
+    function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+        const file = e.target.files?.[0];
+        if (file) {
+            // Create an object URL for the selected file
+            setImageSrc(URL.createObjectURL(file));
+        }
+    }
     return (
         <div className="flex h-screen w-screen flex-wrap bg-white">
             <Sidebar />
@@ -13,7 +25,7 @@ export default function Account() {
                 <div className="b-0 h-f flex h-[93%] w-[100%] flex-col items-center space-y-1 overflow-y-auto bg-blue-300 pb-[1.5%]">
                     <div className="mb-[5%] mt-[5%] flex h-[13%] w-[40%] flex-row items-center justify-center lg:mb-[1%] lg:mt-0 lg:h-[18%]">
                         <Image
-                            src={Cat}
+                            src={imageSrc}
                             alt="pfp"
                             height={0}
                             width={90}
@@ -29,6 +41,7 @@ export default function Account() {
                             type="file"
                             id="image-upload"
                             accept="image/*"
+                            onChange={handleChange}
                             className="hidden"
                         />
                         <h1 className="ml-[2%] mt-[4%] text-center text-4xl text-black">
@@ -64,12 +77,14 @@ export default function Account() {
                                 className="h-7 w-[50%] pl-[0.5%] text-lg text-black outline-double"
                             />
                         </div>
-                        <div className="flex h-auto w-auto flex-row items-center justify-center space-x-5 pr-[3%] lg:w-[90%]">
+                        <div className="mr-[5%] flex h-auto w-auto flex-row items-center justify-center space-x-5 lg:w-[90%]">
                             <label className="text-lg text-black">
                                 Phone Number
                             </label>
                             <input
-                                type="text"
+                                type="tel"
+                                placeholder="+1 45 678 1234"
+                                pattern="[0-9]{1,3}-[0-9]{2}-[0-9]{3}-[0-9]{4}"
                                 className="h-7 w-[50%] pl-[0.5%] text-lg text-black outline-double"
                             />
                         </div>
